c33643d54b9103482bfd088bef0ee3bb915b5242
Benjamin Herrera Navarro <bherreranavarro@winsystems.com>
2024-06-06 18:30:01 +0000

    Fix Serial Scripts: names and syntax

--- recipes-winsys/useradd/useradd-winsys/rs422uart1.py
+++ recipes-winsys/useradd/useradd-winsys/rs422uart1.py
@@ -3,37 +3,43 @@ import gpiod
 import sys
 import time
 
-# Get GPIO0 Bank/gpiochip0
+subprocess.call(['dmesg', '-n', '1'])
+subprocess.call(['systemctl', 'stop', 'serial-getty@ttymxc0.service'])
+
+# GPIO Banks are 0 indexed by the kernel: GPIO1 is Bank/gpiochip0
 chip=gpiod.chip('gpiochip2')
 
+# These are the GPIO lines connected to the UART1 transciever.
+# MX8MQ_IOMUXC_NAND_CE2_B_GPIO3_IO3     Serial SHDN
+# MX8MQ_IOMUXC_NAND_CE3_B_GPIO3_IO4     Serial SLEW
+# MX8MQ_IOMUXC_NAND_DQS_GPIO3_IO14      Serial FD_TX
+# MX8MQ_IOMUXC_NAND_RE_B_GPIO3_IO15     Serial TERM
+# MX8MQ_IOMUXC_NAND_READY_B_GPIO3_IO16  Serial MODE
+# MX8MQ_IOMUXC_NAND_WE_B_GPIO3_IO17     Serial HALF/FULL DUPLEX
+# MX8MQ_IOMUXC_NAND_WP_B_GPIO3_IO18     Serial READ ENABLE
+
 # Get gpiochip0 bank lines
-gpio80 = chip.get_line(16)
-gpio81 = chip.get_line(17)
-gpio82 = chip.get_line(18)
+gpio16 = chip.get_line(16) #RS485 EN
+gpio17 = chip.get_line(17) #Full Duplex
+gpio18 = chip.get_line(18) #Read Enable ACTIVE LOW
+gpio4  = chip.get_line(4)  #SLEW ALLOWS HIGHER BAUD RATES ABOVE 0.5M
 
 # Create Output config
 config_output = gpiod.line_request()
-config_output.consumer = "RS422UART2"
+config_output.consumer = "RS485UART1"
 config_output.request_type = gpiod.line_request.DIRECTION_OUTPUT
 
-gpio80.request(config_output, 0)
-gpio81.request(config_output, 0)
-gpio82.request(config_output, 0)
+# Request Lines
+gpio16.request(config_output, 0)
+gpio17.request(config_output, 0)
+gpio18.request(config_output, 0)
+gpio4.request(config_output, 0)
 
 #Set GPIO values
-gpio80.set_value(1)
-gpio81.set_value(0)
-gpio82.set_value(0)
+gpio16.set_value(1) #RS485 ENABLE
+gpio17.set_value(0) #Full Duplex
+gpio18.set_value(0) #ACTIVE LOW READ ENABLE
+gpio4.set_value(0)  #ACTIVE HIGH SLEW ENABLE FOR HIGH BAUD RATE SPEEDS
 
-subprocess.call(['./rs485-config', '2', 'nostdout'])
-
-# Old legacy bash script64
-# echo 80 > /sys/class/gpio/export
-# echo 81 > /sys/class/gpio/export
-# echo 82 > /sys/class/gpio/export
-# echo out > /sys/class/gpio/gpio80/direction
-# echo out > /sys/class/gpio/gpio81/direction
-# echo out > /sys/class/gpio/gpio82/direction
-# echo 1 > /sys/class/gpio/gpio80/value
-# echo 0 > /sys/class/gpio/gpio81/value
-# echo 0 > /sys/class/gpio/gpio82/value
+# The rs485-config file now is 0 indexed to match kernel device indexing.
+subprocess.call(['./rs485-config', '0'])
--- recipes-winsys/useradd/useradd-winsys/rs485uart2.py
+++ recipes-winsys/useradd/useradd-winsys/rs485uart2.py
@@ -3,44 +3,42 @@ import gpiod
 import sys
 import time
 
-subprocess.call(['dmesg', '-n', '1'])
-subprocess.call(['systemctl', 'stop', 'serial-getty@ttymxc0.service'])
-
-# Get GPIO0 Bank/gpiochip0
+# GPIO Banks are 0 indexed by the kernel: GPIO1 is Bank/gpiochip0
 chip=gpiod.chip('gpiochip2')
 
-# Get gpiochip0 bank lines
-gpio73 = chip.get_line(9) #RS485 EN
-gpio72 = chip.get_line(8) #Full Duplex
-gpio71 = chip.get_line(7)
+# These are the GPIO lines connected to the UART2 transciever.
+# MX8MQ_IOMUXC_NAND_DATA07_GPIO3_IO13     Serial SHDN
+# MX8MQ_IOMUXC_NAND_DATA06_GPIO3_IO12     Serial SLEW
+# MX8MQ_IOMUXC_NAND_DATA05_GPIO3_IO11     Serial FD_TX
+# MX8MQ_IOMUXC_NAND_DATA04_GPIO3_IO10     Serial TERM
+# MX8MQ_IOMUXC_NAND_DATA03_GPIO3_IO9      Serial MODE
+# MX8MQ_IOMUXC_NAND_DATA02_GPIO3_IO8      Serial HALF/FULL DUPLEX
+# MX8MQ_IOMUXC_NAND_DATA01_GPIO3_IO7      Serial READ ENABLE
+
+# Get gpiochip2 bank lines
+gpio9   = chip.get_line(9)  #RS485 EN
+gpio8   = chip.get_line(8)  #Full Duplex
+gpio7   = chip.get_line(7)  #Read Enable ACTIVE LOW
+gpio22  = chip.get_line(12) #SLEW ALLOWS HIGHER BAUD RATES ABOVE 0.5M
+
 
 # Create Output config
 config_output = gpiod.line_request()
-config_output.consumer = "RS485UART1"
+config_output.consumer = "RS485UART2"
 config_output.request_type = gpiod.line_request.DIRECTION_OUTPUT
 
+# Request Lines
+gpio9   = chip.request(config_output, 0)
+gpio8   = chip.request(config_output, 0)
+gpio7   = chip.request(config_output, 0)
+gpio22  = chip.request(config_output, 0)
 
-gpio73.request(config_output, 0)
-gpio72.request(config_output, 0)
-gpio71.request(config_output, 0)
+# Set GPIO values
+gpio9.set_value(1)  #RS485 ENABLE
+gpio8.set_value(1)  #Full Duplex
+gpio7.set_value(0)  #ACTIVE LOW READ ENABLE
+gpio22.set_value(1) #ACTIVE HIGH SLEW ENABLE FOR HIGH BAUD RATE SPEEDS
 
-#Set GPIO values
-gpio73.set_value(1)
-gpio72.set_value(1)
-gpio71.set_value(0)
+# The rs485-config file now is 0 indexed to match kernel device indexing.
+subprocess.call(['./rs485-config', '1'])
 
-#The rs485-config file now is 0 indexed.
-subprocess.call(['./rs485-config', '0'])
-
-# Old legacy bash script
-# echo 73 > /sys/class/gpio/export
-# echo 72 > /sys/class/gpio/export
-# echo 71 > /sys/class/gpio/export
-# echo out > /sys/class/gpio/gpio73/direction
-# echo out > /sys/class/gpio/gpio72/direction
-# echo out > /sys/class/gpio/gpio71/direction
-# echo 1 > /sys/class/gpio/gpio73/value
-# echo 1 > /sys/class/gpio/gpio72/value
-# echo 0 > /sys/class/gpio/gpio71/value
-
-#./rs485-config 1
